a4e5cb96b41d6c844929192d173fe74796a470da
Paweł Korniak <pawello1020@gmail.com>
2021-01-12 14:10:36 +0100

    3 to 2

--- app.java
+++ app.java
@@ -3,7 +3,7 @@ public class FirstJavaContainer {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Starting");
-		for (int i=0; i<3; i++) {
+		for (int i=0; i<2; i++) {
 			System.out.println("Enter your name: ");
 			String s = in.nextLine();
 			System.out.println("Hello " + s);
